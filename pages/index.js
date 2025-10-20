d9dcefa51c163efb1bdefa26df087214ed244af5
Gustavo Henrique <gustavohenrique@pactosolucoes.com.br>
2025-10-20 10:47:00 -0300

    adiciona scripts `linst:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Eu vou me doar 101% todos os dias para que nossa família vença e conquiste tudo que desejamos (com aprovação de Deus). Obrigado por acreditar em mim ❤️</h1>
+  return (
+    <h1>
+      Eu vou me doar 101% todos os dias para que nossa família vença e conquiste
+      tudo que desejamos (com aprovação de Deus). Obrigado por acreditar em mim
+      ❤️
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
